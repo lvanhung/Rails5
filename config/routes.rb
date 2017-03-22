c5a65f207efbe64eb3751775259bc7c473841596
admin <admin@admins-MacBook-Pro-2.local>
2017-03-22 11:34:35 +0700

    add devise to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users, controllers: {
+  #     sessions: 'users/sessions'
+  # }
+
+  # devise_for :users, path: 'users', path_names:
+  #     { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification',
+  #       unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
 
-  root 'welcome#index'
+  #root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
