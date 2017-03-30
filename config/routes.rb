dfe59c447014ab306a55f846e849c58e1d3fb1fe
admin <admin@admins-MacBook-Pro-2.local>
2017-03-30 16:50:35 +0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,6 @@ Rails.application.routes.draw do
   resources :books
   resource :messages
   devise_for :users
-  # devise_for :users, controllers: {
-  #     sessions: 'users/sessions'
-  # }
-
-  # devise_for :users, path: 'users', path_names:
-  #     { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification',
-  #       unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-
 
   get 'static_pages/home'
 
@@ -25,5 +17,8 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   root 'welcome#index'
+
+  mount ActionCable.server, at: '/cable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
